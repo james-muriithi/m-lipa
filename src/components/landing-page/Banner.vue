1d147f773dde4f87f18f7e0c69ccd9116bad43d2
james-muriithi <muriithijames556@gmail.com>
2022-09-24 01:08:23 +0300

    added banner slogan and action button

--- src/components/landing-page/Banner.vue
+++ src/components/landing-page/Banner.vue
@@ -1,5 +1,23 @@
 <template>
-  <section id="banner" class="h-100"></section>
+  <section id="banner" class="h-100 pt-5 container-xl">
+    <div class="mt-5 row h-75 align-items-center">
+      <div class="col-sm-6">
+        <h3 class="fs-1 fw-bold">
+          Simplifying your <span class="text-primary d-block">finance</span>
+        </h3>
+        <p class="text-muted pe-5 mt-4">
+          Fast, easier and cheaper cross border
+          <span class="d-lg-block">payments starts here.</span>
+        </p>
+        <div class="mt-4">
+            <router-link to="#" class="btn btn-primary rounded-pill px-4 fw-semibold fs-7 py-2">
+                Get Started now
+            </router-link>
+        </div>
+      </div>
+      <div class="col-md-6"></div>
+    </div>
+  </section>
 </template>
 
 <script>
