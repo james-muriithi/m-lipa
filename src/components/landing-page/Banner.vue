aa0fc9379614fa84b466bc766137626c2472e1da
james-muriithi <muriithijames556@gmail.com>
2022-09-24 01:30:48 +0300

    added banner bottom cards

--- src/components/landing-page/Banner.vue
+++ src/components/landing-page/Banner.vue
@@ -1,7 +1,7 @@
 <template>
   <section id="banner" class="h-100 pt-5 container-xl">
-    <div class="mt-5 row h-75 align-items-center">
-      <div class="col-sm-6">
+    <div class="pt-5 row h-75 align-items-center">
+      <div class="col-sm-6 col-md-5">
         <h3 class="fs-1 fw-bold">
           Simplifying your <span class="text-primary d-block">finance</span>
         </h3>
@@ -10,12 +10,40 @@
           <span class="d-lg-block">payments starts here.</span>
         </p>
         <div class="mt-4">
-            <router-link to="#" class="btn btn-primary rounded-pill px-4 fw-semibold fs-7 py-2">
-                Get Started now
-            </router-link>
+          <router-link
+            to="#"
+            class="btn btn-primary rounded-pill px-4 fw-semibold fs-7 py-2"
+          >
+            Get Started now
+          </router-link>
         </div>
       </div>
-      <div class="col-md-6"></div>
+      <div class="col-md-7 col-sm-6 h-100">
+        <img
+          class="img-fluid h-100"
+          src="@/assets/landing-page/banner-illustration.svg"
+        />
+      </div>
+    </div>
+    <div class="banner-cards h-25 py-4">
+      <div class="row h-100">
+        <div class="col-md-4 h-100" v-for="i in 3" :key="i">
+          <div class="h-100 card-item w-100 px-4 py-3 shadow">
+            <p class="d-flex align-items-center fw-bold fs-7 fs-lg-6 mb-1">
+              <span class="material-icons text-secondary fs-1"
+                >people_outline</span
+              >
+              <span class="ps-2">The quick fox jumps over the lazy dog</span>
+            </p>
+            <p class="text-muted fs-7 mb-0">
+              the quick fox jumps over the lazy dog the quick fox jumps over the
+              lazy dog
+            </p>
+          </div>
+        </div>
+        <div class="col-md-4"></div>
+        <div class="col-md-4"></div>
+      </div>
     </div>
   </section>
 </template>
